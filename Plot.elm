b07e2818cb0d02e2ba5883d516fffff7d0ae5ad8
Erik Broman <erik.broman@me.com>
2014-12-16 22:48:29 +0100

    Added y axis

--- Plot.elm
+++ Plot.elm
@@ -53,8 +53,8 @@ plotConfig f (from,to) steps =
        ys = List.map f xs
        dx = xExtent / (toFloat steps)
        xScale = C.normalize xDomain
-       baselinePos = if (List.minimum ys < 0) then List.minimum ys else 0 
-       baselineNeg = if (List.maximum ys > 0) then List.maximum ys else 0 
+       baselinePos = if (List.minimum ys < 0) then List.minimum ys else -0.5 
+       baselineNeg = if (List.maximum ys > 0) then List.maximum ys else 0.5 
        yScale = C.normalize (baselinePos, baselineNeg)
        ymin = List.minimum ys
        ymax = List.maximum ys
@@ -79,7 +79,7 @@ plotConfig f (from,to) steps =
 
 --pc = plotConfig (\x -> logBase e x) (1,2*pi) 100
 --pc = plotConfig (\x -> 6 * sin x) (-2*pi,2*pi) 100
-pc = plotConfig (\x -> cos x) (-2*pi,2*pi) 100
+pc = plotConfig (\x -> 0.5 * (cos x)) (-2*pi,2*pi) 100
 --pc = plotConfig (\x -> e^x) (-2*pi,2*pi) 100
 --pc = plotConfig (\x -> C.pdfstandardnormal x) (-2*pi,2*pi) 100
 --pc = Signal.map (plotConfig sin (-2*pi,2*pi)) Mouse.x 
@@ -107,13 +107,16 @@ bar (xm,ym) (xscale, yscale) f (x1',x2) =
 
 geom_bar cfg (xm,ym) (dx', steps) = 
    let
+      aes = { colour = Color.blue, 
+              linestyle = GC.solid
+            }
       xscale = cfg.xScale
       yscale = cfg.yScale
       dx = dx' / 2
       xs = C.bins <| List.map (\x -> x - dx) steps
       points = List.map (bar (xm,ym) (xscale,yscale) cfg.f) xs 
    in
-      GC.group <| List.map (\x -> GC.outlined (GC.solid Color.blue) <| GC.polygon x) points
+      GC.group <| List.map (\x -> GC.outlined (aes.linestyle aes.colour) <| GC.polygon x) points
 
 
 {-| Calculating trapezium points for plotting them as polygons. -}
@@ -123,64 +126,98 @@ trapezoid (xm,ym) (xscale, yscale) f (x1,x2) =
 
 geom_trapezoid cfg (xm,ym) (dx, steps) = 
    let
+      aes = { colour = Color.blue, 
+              linestyle = GC.solid
+            }
       xscale = cfg.xScale
       yscale = cfg.yScale
       xs = C.bins steps
       points = List.map (trapezoid (xm,ym) (xscale,yscale) cfg.f) xs 
    in
-      GC.group <| List.map (\x -> GC.outlined (GC.solid Color.blue) <| GC.polygon x) points
+      GC.group <| List.map (\x -> GC.outlined (aes.linestyle aes.colour) <| GC.polygon x) points
 
 geom_curve cfg (xm,ym) nsteps = 
    let
+      aes = { colour = Color.darkGrey, 
+               linestyle = GC.solid
+            }
       xscale = cfg.xScale
       yscale = cfg.yScale
       (dx,steps) = C.interpolate cfg.xDomain (toFloat nsteps) cfg.f
       ys = List.map (\(x,y) -> (xm * (xscale x),ym * (yscale y))) <| List.map2 (,) steps (List.map cfg.f steps)
    in
-      GC.traced (GC.solid Color.darkGrey) <| GC.path ys
+      GC.traced (aes.linestyle aes.colour) <| GC.path ys
 
 geom_hline cfg (xm, ym) y =
-      GC.traced (GC.solid Color.black)  
+   let
+       aes = { colour = Color.black, 
+               linestyle = GC.solid
+            }
+   in
+      GC.traced (aes.linestyle aes.colour)  
       <| GC.path [(0,ym * (cfg.yScale y)), (xm, ym * (cfg.yScale y))] 
 
 geom_vline cfg (xm, ym) x =
-      GC.traced (GC.solid Color.black)  
+   let
+       aes = { colour = Color.black, 
+               linestyle = GC.solid
+            }
+   in
+      GC.traced (aes.linestyle aes.colour)  
       <| GC.path [(xm * (cfg.xScale x),0), (xm * (cfg.xScale x), ym)] 
 
 
 geom_abline cfg (xm,ym) x' = 
    let
+       aes = { annotationPosition = (80,50), 
+               colour = Color.red, 
+               dx = 0.033
+            }
        xmin = fst cfg.xDomain
        ymin = fst cfg.yDomain
        ymax = snd cfg.yDomain
-       --ymin = cfg.yScale <| fst cfg.yDomain
-       dx = cfg.dx / 4
+       --dx = cfg.dx / 4
+       dx = aes.dx
        x = xmin + (cfg.xExtent * x')
-       tangent = C.tangent cfg.dx x cfg.f
+       tangent = C.tangent dx x cfg.f
        m = tangent.slope
        b = tangent.intercept
        fun = (\x -> m * x + b)
-       --y = ymin + (fun x)
-       --x1 = xmin + (cfg.xExtent * (x' - dx))
-       --y1 = ymin + (fun x1) - ((1 + cfg.yExtent) / 8)
-       --x2 = xmin + (cfg.xExtent * (x' + dx))
-       --y2 = ymin + (fun x2) - ((1 + cfg.yExtent) / 8)
        y = (fun x)
        x1 = xmin + (cfg.xExtent * (x' - dx))
        y1 = fun x1
        x2 = xmin + (cfg.xExtent * (x' + dx))
        y2 = fun x2
-       ys = [(xm * (cfg.xScale x1), ym * (cfg.yScale y1) - 200),
-             (xm * (cfg.xScale x2), ym * (cfg.yScale y2) - 200)]
-       derivative = GC.move (80,50) <| GC.toForm <| Text.leftAligned <| Text.fromString  
+       ys = [(xm * (cfg.xScale x1), ym * (cfg.yScale y1)),
+             (xm * (cfg.xScale x2), ym * (cfg.yScale y2))]
+       annotation = GC.move aes.annotationPosition <| GC.toForm <| Text.leftAligned <| Text.fromString  
             <| "slope: " ++ (toString m) ++ "x\nintercept: " ++ (toString <| C.dec 3 b)
    in
         --GC.move (80,300) <| GC.toForm <| Text.rightAligned <| Text.fromString <| toString <| List.map (\(a,b) -> (C.dec 2 a, C.dec 2 b)) ys
         --GC.move (80,300) <| GC.toForm <| Text.rightAligned <| Text.fromString <| toString <| tangent
         --GC.move (80,300) <| GC.toForm <| Text.rightAligned <| Text.fromString <| toString <| y
         --GC.move (80,280) <| GC.toForm <| Text.rightAligned <| Text.fromString  <| (toString <| (C.dec 2 x1,C.dec 2 x,C.dec 2 x2, C.dec 2 (x2 - x1))) ++ "\n" ++ (toString <| (C.dec 2 y1,C.dec 2y,C.dec 2 y2, C.dec 2 (y2 - y1))) ++ "\n" ++ (toString (C.dec 2 ymin, C.dec 2 ymax))
-       GC.group [GC.move (0,200) <| GC.traced (GC.solid Color.red) <| GC.path ys, GC.move (xm * cfg.xScale x,ym * cfg.yScale (cfg.f x)) <| GC.filled Color.red <| GC.circle 2, derivative]
+       GC.group [GC.traced (GC.solid Color.red) <| GC.path ys,  
+       GC.move (xm * cfg.xScale x,ym * cfg.yScale (cfg.f x))  
+       <| GC.filled aes.colour <| GC.circle 2,  
+       annotation]
        
+axisY cfg (xm,ym) xmargin = 
+   let
+       xmin = fst cfg.xDomain
+       ymin = fst cfg.yDomain
+       ymax = snd cfg.yDomain
+       pos = (xm * (cfg.xScale xmin) - (xmargin / 4),0)
+       tickPositions = [ymin, 0, ymax]
+       tickLabels = GC.group <| List.map (\y -> GC.move (-xmargin / 8,ym * cfg.yScale y)  
+         <| GC.toForm <| Text.rightAligned <| Text.fromString <|  
+         toString <| C.dec 2 y) tickPositions
+       ticks = GC.group <| List.map (\y -> GC.move (-xmargin * 0.02,ym * cfg.yScale y)  
+         <| GC.traced (GC.solid Color.black) <| GC.path [(0,0),(12,0)]) tickPositions
+       yBar = GC.traced (GC.solid Color.black) <| GC.path [(10,0),(10,ym)]
+   in
+      GC.move pos <| GC.group  
+      [tickLabels, ticks, yBar]
 
 --meh (mx,my) (ww,wh) = Text.asText <| (C.normalize (0,toFloat ww) (toFloat mx))
 --main = Signal.map2 meh Mouse.position Window.dimensions
@@ -188,14 +225,15 @@ geom_abline cfg (xm,ym) x' =
 main = Signal.map3 (plotc (1400,400) (\x -> -3 + sin x) (-2*pi,2*pi) pc) (Signal.constant (-2*pi,2*pi)) Mouse.x Window.dimensions 
 --main = Signal.map Text.asText Window.dimensions
 
-plotc (plotWidth,plotHeight) f (xmin,xmax) cfg (from,to) nbins' (windowWidth,windowHeight) =  
+plotc (plotWidth,plotHeight) f (xmin,xmax) cfg (from,to) mouseX (windowWidth,windowHeight) =  
    let
       maxbins = 400
       windowScale = C.normalize (0,toFloat windowWidth)
-      nbins = round <| (toFloat nbins') / 4
-      --xpos = (fst cfg.xDomain) + cfg.xExtent * windowScale (toFloat nbins')
-      wpos = windowScale (toFloat nbins')
+      nbins = round <| (toFloat mouseX) / 4
+      --xpos = (fst cfg.xDomain) + cfg.xExtent * windowScale (toFloat mouseX)
+      wpos = windowScale (toFloat mouseX)
       xpos = (fst cfg.xDomain) + cfg.xExtent * wpos
+      ypos = cfg.f <| xpos
       xmargin = (toFloat plotWidth) * 0.1
       ymargin = (toFloat plotHeight) * 0.1
       xoffset = (toFloat plotWidth)/2 - xmargin
@@ -213,8 +251,10 @@ plotc (plotWidth,plotHeight) f (xmin,xmax) cfg (from,to) nbins' (windowWidth,win
       zeroX = geom_hline cfg (xmultiplier,ymultiplier) 0
       zeroY = geom_vline cfg (xmultiplier,ymultiplier) 0
       xMarker = geom_vline cfg (xmultiplier,ymultiplier) xpos
+      yMarker = geom_hline cfg (xmultiplier,ymultiplier) ypos
       tangent = geom_abline cfg (xmultiplier,ymultiplier) wpos
-      yAxis = GC.traced (GC.solid Color.black) <| GC.path [(0,0),(0,ymultiplier)]
+      --yAxis = GC.traced (GC.solid Color.black) <| GC.path [(0,0),(0,ymultiplier)]
+      yAxis = axisY cfg (xmultiplier,ymultiplier) xmargin
       --xAxis = GC.traced (GC.solid Color.black) <| GC.path [(0,0),(xmultiplier,0)]
    in 
       GC.collage plotWidth plotHeight  
@@ -227,11 +267,12 @@ plotc (plotWidth,plotHeight) f (xmin,xmax) cfg (from,to) nbins' (windowWidth,win
                 GC.move (-xoffset, -yoffset) <| geom_curve cfg (xmultiplier,ymultiplier) cfg.steps, 
               --GC.move (-xoffset, -yoffset) <| geom_trapezoid cfg (xmultiplier, ymultiplier) (dx,steps),
               GC.move (-xoffset, -yoffset) <| geom_bar cfg (xmultiplier, ymultiplier) (dx,steps),
-              GC.move (-xoffset, -yoffset) <| geom_point cfg (xmultiplier, ymultiplier) (dx,steps),
-              GC.move (-xoffset - (xmargin / 4), -yoffset) <| yAxis,
+              --GC.move (-xoffset, -yoffset) <| geom_point cfg (xmultiplier, ymultiplier) (dx,steps),
+              GC.move (-xoffset, -yoffset) <| yAxis,
               GC.move (-xoffset, -yoffset) <| zeroX,
               GC.move (-xoffset, -yoffset) <| zeroY,
               GC.move (-xoffset, -yoffset) <| xMarker,
+              GC.move (-xoffset, -yoffset) <| yMarker,
               GC.move (-xoffset, -yoffset) <| tangent,
               --GC.move (-xoffset, -yoffset) <| xAxis,
               GC.move (-xoffset, -yoffset) <| plotFrame,
